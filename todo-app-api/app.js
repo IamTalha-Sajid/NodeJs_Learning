7d63ebad4d4785850dc24bd19f7b26f9d2b68f53
Talha Sajid <talha@aleh.tech>
2023-09-27 17:16:04 +0500

    🔥 Feature: Now data is stored in JSON files and index is called by key value

--- todo-app-api/app.js
+++ todo-app-api/app.js
@@ -1,8 +1,18 @@
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
+const fs = require('fs'); // Import the 'fs' module for file operations
 
-const todos = [];
+const todosFilePath = 'todos.json'; // File path for storing todo data
+
+let todos = []; // Initialize the todos array
+
+// Read todos from the JSON file on server startup
+fs.readFile(todosFilePath, 'utf8', (err, data) => {
+  if (!err) {
+    todos = JSON.parse(data);
+  }
+});
 
 function handleRequest(req, res) {
   const parsedUrl = url.parse(req.url);
@@ -22,13 +32,21 @@ function handleRequest(req, res) {
       req.on('end', () => {
         const newTodo = JSON.parse(data);
         todos.push(newTodo);
+
+        // Update the JSON file with the new data
+        fs.writeFile(todosFilePath, JSON.stringify(todos), (err) => {
+          if (err) {
+            console.error('Error writing to todos.json:', err);
+          }
+        });
+
         res.writeHead(201, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(newTodo));
       });
     }
   } else if (parsedUrl.pathname.startsWith('/todos/')) {
-    const todoIndex = parseInt(parsedUrl.pathname.split('/')[2]);
-
+    
+    const todoIndex = parseInt(query.index);
     if (!isNaN(todoIndex) && todoIndex >= 0 && todoIndex < todos.length) {
       if (req.method === 'GET') {
         // Get a specific todo
@@ -43,12 +61,28 @@ function handleRequest(req, res) {
         req.on('end', () => {
           const editedTodo = JSON.parse(data);
           todos[todoIndex] = editedTodo;
+
+          // Update the JSON file with the edited data
+          fs.writeFile(todosFilePath, JSON.stringify(todos), (err) => {
+            if (err) {
+              console.error('Error writing to todos.json:', err);
+            }
+          });
+
           res.writeHead(200, { 'Content-Type': 'application/json' });
           res.end(JSON.stringify(editedTodo));
         });
       } else if (req.method === 'DELETE') {
         // Delete a specific todo
         todos.splice(todoIndex, 1);
+
+        // Update the JSON file with the updated data
+        fs.writeFile(todosFilePath, JSON.stringify(todos), (err) => {
+          if (err) {
+            console.error('Error writing to todos.json:', err);
+          }
+        });
+
         res.writeHead(204);
         res.end();
       }
